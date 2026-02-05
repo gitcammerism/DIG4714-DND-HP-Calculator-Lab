f16e104356ee9bfd0a03d2f9269fa9ee1c64fe45
Cami Alarcon-Fernandez <camialarconfern@gmail.com>
2026-02-05 11:16:36 -0500

    Initial files

---
+++ SolutionOne.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolutionOne : MonoBehaviour
+{
+    // dictionary with character race and hit die, <string, string>
+    private Dictionary<string, int> raceHitDie = new Dictionary<string, int>()
+    {
+
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
